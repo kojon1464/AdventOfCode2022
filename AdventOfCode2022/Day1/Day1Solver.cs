05432179831371ab2df02cf6e3357f1a4de51dbe
Filip Zmuda <kojonek1464@wp.pl>
2022-12-02 19:46:59 +0100

    Fix .gitignore and push changes

---
+++ AdventOfCode2022/Day1/Day1Solver.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace AdventOfCode2022.Day1
+{
+    internal class Day1Solver
+    {
+        public static void SolveTask1()
+        {
+            using (StreamReader reader = new StreamReader(@"Day1\input1.txt"))
+            {
+                Console.WriteLine(CalculateTopElvesCaloriesSum(reader, 1));
+            }
+        }
+
+        public static void SolveTask2()
+        {
+            using (StreamReader reader = new StreamReader(@"Day1\input2.txt"))
+            {
+                Console.WriteLine(CalculateTopElvesCaloriesSum(reader, 3));
+            }
+        }
+
+        private static int CalculateTopElvesCaloriesSum(StreamReader reader, int numberOfElvesToSum)
+        {
+            int parsedCalories;
+            int calories = 0;
+            var priorityQueue = new PriorityQueue<int, int>( Comparer<int>.Create((int a, int b) => { return b-a; }));
+            var line = reader.ReadLine();
+
+            while (line != null)
+            {
+                if (int.TryParse(line, out parsedCalories))
+                {
+                    calories += parsedCalories;
+                }
+                else
+                {
+                    priorityQueue.Enqueue(calories, calories);
+                    calories = 0;
+                }
+
+                line = reader.ReadLine();
+            }
+
+            priorityQueue.Enqueue(calories, calories);
+
+            int caloriesSum = 0;
+            
+            for(int i = 0; i < numberOfElvesToSum; i++)
+            {
+                caloriesSum += priorityQueue.Dequeue();
+            }
+
+            return caloriesSum;
+        }
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode2022.Day1;
+
+namespace AdventOfCode2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SolveDay1();
+        }
+
+        public static void SolveDay1()
+        {
+            Day1Solver.SolveTask1();
+            Day1Solver.SolveTask2();
+        }
+    }
+}
