4288b749162ccf14d20b50162318ff9c05a8e8a6
kojon1464 <kojonek1464@wp.pl>
2022-12-26 13:12:48 +0100

    Day 7 solved

---
+++ AdventOfCode2022/Day7/Day7Solver.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day7
+{
+    internal class Day7Solver
+    {
+        public static void SolveTask1()
+        {
+            using(StreamReader reader = new StreamReader(@"Day7\input1.txt"))
+            {
+                int directorySizeAtMost100000 = 0;
+                List<int> sizes = new List<int>();
+
+                CheckDirectoriesSize(reader, sizes);
+
+                foreach (var size in sizes)
+                {
+                    if (size <= 100_000)
+                    {
+                        directorySizeAtMost100000 += size;
+                    }
+                }
+
+                Console.WriteLine(directorySizeAtMost100000);
+            }
+        }
+
+        private static int CheckDirectoriesSize(StreamReader reader, List<int> sizes)
+        {
+            int directorySize = 0;
+            bool directoryChecked = false;
+
+            string? line;
+
+            do
+            {
+                line = reader.ReadLine();
+                object outout = line switch
+                {
+                    null => directoryChecked = true,
+                    var str when str.StartsWith("$ cd ..") => directoryChecked = true,
+                    var str when str.StartsWith("$ cd ") => directorySize += CheckDirectoriesSize(reader, sizes),
+                    var str when Regex.IsMatch(str, "^\\d") => directorySize += GetDirectorySize(str),
+                    _ => true
+                };
+            }
+            while (!directoryChecked);
+
+            sizes.Add(directorySize);
+
+            return directorySize;
+        }
+
+        private static int GetDirectorySize(string line)
+        {
+            int size;
+            var words = line.Split(' ');
+
+            int.TryParse(words[0], out size);
+
+            return size;
+        }
+
+        public static void SolveTask2() 
+        {
+            using (StreamReader reader = new StreamReader(@"Day7\input1.txt"))
+            {
+                int diskSpace = 70_000_000;
+                int spaceRequired = 30_000_000;
+
+                int smallest = int.MaxValue;
+                List<int> sizes = new List<int>();
+
+                CheckDirectoriesSize(reader, sizes);
+
+                int spaceTaken = sizes.Max();
+                int spaceToFree = spaceRequired - (diskSpace - spaceTaken);
+
+                foreach (var size in sizes)
+                {
+                    if (size < smallest && size >= spaceToFree)
+                    {
+                        smallest = size;
+                    }
+                }
+
+                Console.WriteLine(smallest);
+            }
+        }
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -4,6 +4,7 @@ using AdventOfCode2022.Day3;
 using AdventOfCode2022.Day4;
 using AdventOfCode2022.Day5;
 using AdventOfCode2022.Day6;
+using AdventOfCode2022.Day7;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace AdventOfCode2022
@@ -17,7 +18,8 @@ namespace AdventOfCode2022
             //SolveDay3();
             //SolveDay4();
             //SolveDay5();
-            SolveDay6();
+            //SolveDay6();
+            SolveDay7();
         }
 
         public static void SolveDay1()
@@ -54,6 +56,12 @@ namespace AdventOfCode2022
         {
             Day6Solver.SolveTask1();
             Day6Solver.SolveTask2();
+        }
+
+        public static void SolveDay7()
+        {
+            Day7Solver.SolveTask1();
+            Day7Solver.SolveTask2();
         }
     }
 }
